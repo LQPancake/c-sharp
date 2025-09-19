e309bd8f16c2ce783a53edfc22cdbc178fa3da83
Pancake <baligajozsef@gmail.com>
2025-09-19 08:17:50 +0200

    -

--- szallas/szallas/Program.cs
+++ szallas/szallas/Program.cs
@@ -19,27 +19,50 @@ namespace szallas
         static void Main(string[] args)
         {
             Console.Write("4. feladat:");
+            var szallasok = beOlvas();
+            Console.WriteLine("Az összes szálláshelyek száma: " + szallasok.Count);
             Console.WriteLine("5. feladat:");
+            var aktiv = szallasok.Where(x => x.statusz);
+            double atlagAgy = aktiv.Average(x => x.agySzam);
+            Console.WriteLine("Átlagos ágyszám (aktív): " + atlagAgy);
             Console.WriteLine("6. feladat:");
-
+            Console.WriteLine("Szeghalom környéki aktív szállások:");
+            var szeghalomAktiv = szallasok.Where(x => x.statusz && x.cim.Contains("Szeghalom")); foreach (var sz in szeghalomAktiv)
+            {
+                Console.WriteLine($"{sz.nev} - {sz.cim}");
+            }
             Console.ReadLine();
         }
-        public static void beOlvas(string fileName = "szallas2022.csv")
+        public static List<Szallas> beOlvas(string fileName = "szallas2022.csv")
         {
+            List<Szallas> szallasok = new List<Szallas>();
             try
             {
-                string[] sorok = File.ReadAllLines(fileName, Encoding.UTF8);
-                foreach (var sor in sorok)
+                StreamReader sr = new StreamReader(fileName, Encoding.UTF8);
+                using (sr)
                 {
-                    StreamReader Sr = new StreamReader(fileName, Encoding.UTF8);
-                    List<Szallas> szallasok = new List<Szallas>();
-                    sor[] = Sr.ReadLine();
+                    string sor = sr.ReadLine();
+                    string[] mezok = sor.Split(';');
+
+                    Szallas sz = new Szallas
+                    {
+                        nev = mezok[0],
+                        cim = mezok[1],
+                        szobaSzam = Convert.ToInt32(mezok[2]),
+                        agySzam = Convert.ToInt32(mezok[3]),
+                        uzNev = mezok[4],
+                        uzCim = mezok[5],
+                        uzTelSzam = mezok[6],
+                        statusz = mezok[7].Trim() == "Aktív",
+                        tipus = mezok[8],
+                    };
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Hiba van a kódban!", ex.Message);
             }
+            return szallasok;
         }
     }
 
