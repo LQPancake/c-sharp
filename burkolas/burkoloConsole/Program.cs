7576305b56f52f1549767c94f9e9bbb4a2af4032
Pancake <baligajozsef@gmail.com>
2025-09-29 13:14:59 +0200

    -

---
+++ burkolas/burkoloConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace burkoloConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Helyiseg szoba = new Helyiseg();
+            szoba.Name = "Hálószoba";
+            szoba.Description = "padlóburkolat";
+            szoba.Length = 4.18;
+            szoba.Width = 3.5;
+            Console.WriteLine($"{szoba.Name}, terület: {szoba.Length * szoba.Width}m2");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ burkolas/burkoloConsole/helyiseg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace burkoloConsole
+{
+    internal class Helyiseg
+    {
+        public string Name;
+        public string Description;
+        public double Length;
+        public double Width;
+    }
+}
