81ff08822702a0bb31456e5f7d424a94a6c50a6e
Pancake <baligajozsef@gmail.com>
2025-10-06 09:31:18 +0200

    -

--- burkolas/burkoloConsole/Program.cs
+++ burkolas/burkoloConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +11,12 @@ namespace burkoloConsole
     {
         static void Main(string[] args)
         {
-            Helyiseg szoba = new Helyiseg();
-            szoba.Name = "Hálószoba";
-            szoba.Description = "padlóburkolat";
-            szoba.Length = 4.18;
-            szoba.Width = 3.5;
-            Console.WriteLine($"{szoba.Name}, terület: {szoba.Length * szoba.Width}m2");
+            var placeList = Helyiseg.DataLoader();
+            foreach (Helyiseg h in placeList)
+            {
+                Console.WriteLine($"{h.Name} terület: {h.Area()}m\u00B2");
+            }
+            Console.WriteLine($"A 3,5 * 4.5 m-es szoba területe: {Helyiseg.AreaStatic(3.5, 4.5)}\u00B2");
             Console.ReadKey();
         }
     }
--- burkolas/burkoloConsole/helyiseg.cs
+++ burkolas/burkoloConsole/helyiseg.cs
@@ -12,5 +12,43 @@ namespace burkoloConsole
         public string Description;
         public double Length;
         public double Width;
+        public int Perimeter()
+        {
+            return 2 * (int)(Length + Width);
+        }
+        public double Area()
+        {
+            return Length * Width;
+        }
+        public static double AreaStatic(double a, double b)
+        {
+            return a + b;
+        }
+        public static List<Helyiseg> DataLoader()
+        {
+            List<Helyiseg> placeList = new List<Helyiseg>();
+            while (true)
+            {
+                Console.WriteLine("Add meg egy helyiség adatait!");
+                Console.Write("Neve: ");
+                string roomName = Console.ReadLine();
+                if (roomName == "") break;
+                Console.Write("Megjegyzés: ");
+                string roomDesc = Console.ReadLine();
+                Console.Write("Hossz: ");
+                double roomLength = double.Parse(Console.ReadLine());
+                Console.Write("Szélesség: ");
+                double roomWidth = double.Parse(Console.ReadLine());
+
+                Helyiseg szoba = new Helyiseg();
+                szoba.Name = roomName;
+                szoba.Description = roomDesc;
+                szoba.Length = roomLength;
+                szoba.Width = roomWidth;
+                placeList.Add(szoba);
+            }
+            return placeList;
+
+        }
     }
 }
