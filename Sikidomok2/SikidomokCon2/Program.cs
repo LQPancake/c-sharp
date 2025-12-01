df98f1b859f78440fde499d6ab1d787c87807531
Pancake <baligajozsef@gmail.com>
2025-12-01 12:32:12 +0100

    -

--- Sikidomok2/SikidomokCon2/Program.cs
+++ Sikidomok2/SikidomokCon2/Program.cs
@@ -11,36 +11,87 @@ namespace SikidomokCon2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1. rész: Kör adatok gyűjtése\n" +
+            Console.WriteLine("Kör vagy téglalap? (k/t)");
+            string valaszInput = Console.ReadLine();
+            if (valaszInput.ToLower() == "k")
+            {
+                // Kör
+                Console.WriteLine("1. rész: Kör adatok gyűjtése\n" +
                 "\tAdd meg a kör sugarát (üres ENTER=vége)\n");
-            while (true)
-            {
-                Console.Write("---> ");
-                string input = Console.ReadLine();
-                if (input == "") { break; }
-                try
+                while (true)
                 {
-                    double sugar = double.Parse(input);
-                    if (sugar > 0)
+                    Console.Write("---> ");
+                    string input = Console.ReadLine();
+                    if (input == "") { break; }
+                    try
                     {
-                        //példányosítás
-                        var ujObj = new Kor(sugar);
+                        double sugar = double.Parse(input);
+                        if (sugar > 0)
+                        {
+                            //példányosítás
+                            var ujObj = new Kor(sugar);
+                        }
+                        else { continue; }
                     }
-                    else { continue; }
+                    catch
+                    {
+                        Console.WriteLine("Helyes adatokat kérek!");
+                    }
                 }
-                catch
+                Console.WriteLine("\nKörök adatainak listázása\n");
+                foreach (var kor in Kor.KorokListaja)
                 {
-                    Console.WriteLine("Helyes adatokat kérek!");
+                    Console.WriteLine($"A(z) {kor.Sugar} sugarú kör kerülete: {kor.KorKerulet()} Területe: {kor.KorTerulet()}");
                 }
+                Console.WriteLine($"\nÖsszes kerület: {Kor.OsszesKerulet}\n" +
+                    $"Összes terület: {Kor.OsszesTerulet}");
+
             }
-            Console.WriteLine("\nKörök adatainak listázása\n");
-            foreach(var kor in Kor.KorokListaja)
+            else if (valaszInput.ToLower() == "t")
             {
-                Console.WriteLine($"A(z) {kor.Sugar} sugarú kör kerülete: {kor.KorKerulet()} Területe: {kor.KorTerulet()}");
+                // Téglalap
+                Console.WriteLine("2. rész: Téglalap adatok gyűjtése\n" +
+                        "\t Add meg a téglalap 'a' oldalát (üres ENTER=vége)\n");
+                while (true)
+                {
+                    Console.Write("Add meg a téglalap 'a' oldalát (ENTER=vége)\n---> ");
+                    string aInput = Console.ReadLine();
+                    if (aInput == "") { break; }
+
+                    Console.Write("Add meg a téglalap 'b' oldalát\n---> ");
+                    string bInput = Console.ReadLine();
+
+                    try
+                    {
+                        double aOldal = double.Parse(aInput);
+                        double bOldal = double.Parse(bInput);
+
+                        if (aOldal > 0 && bOldal > 0)
+                        {
+                            var ujObj2 = new Teglalap(aOldal, bOldal);
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Helyes adatokat kérek!");
+                    }
+                }
+
+                Console.WriteLine("\nTéglalapok adatainak listázása\n");
+                foreach (var teglalap in Teglalap.TeglalapokListaja)
+                {
+                    Console.WriteLine($"A(z) {teglalap.aOldal} a oldalú és {teglalap.bOldal} b oldalú téglalap kerülete: {teglalap.TeglalapKerulet()}\n" +
+                        $"területe: {teglalap.TeglalapTerulet()} ");
+                }
+                Console.WriteLine($"\nÖsszes kerület: {Teglalap.OsszesKerulet}\n" +
+                    $"Összes terület: {Teglalap.OsszesTerulet}");
+
             }
-            Console.WriteLine($"\nÖsszes kerület: {Kor.OsszesKerulet}\n" +
-                $"Összes terület: {Kor.OsszesTerulet}");
-            Console.ReadKey();
+            else
+            {
+                Console.WriteLine("Helytelen válasz:((((((((((((((((((((((");
+            }
+                Console.ReadKey();
         }
     }
 }
--- Sikidomok2/SikidomokLibrary/SikidomokLibrary.cs
+++ Sikidomok2/SikidomokLibrary/SikidomokLibrary.cs
@@ -13,6 +13,10 @@ namespace SikidomokLibrary
         public static List<Kor> KorokListaja = new List<Kor>();
         public static double OsszesKerulet { get; private set; } = 0;
         public static double OsszesTerulet { get; private set; } = 0;
+        /// <summary>
+        /// Kör objektumot hoz létre
+        /// </summary>
+        /// <param name="sugar">A kör sugara, double típus</param>
         public Kor(double sugar)
         {
             this.Sugar = sugar;
@@ -27,6 +31,35 @@ namespace SikidomokLibrary
         public double KorTerulet()
         {
             return Math.Round(Math.Pow(this.Sugar, 2) * Math.PI, 2);
+        }
+    }
+    public class Teglalap
+    {
+        public double aOldal { get; private set; }
+        public double bOldal { get; private set; }
+        public static List<Teglalap> TeglalapokListaja = new List<Teglalap>();
+        public static double OsszesKerulet { get; private set; } = 0;
+        public static double OsszesTerulet { get; private set; } = 0;
+        /// <summary>
+        /// Téglalap objektumot hoz létre
+        /// </summary>
+        /// <param name="aOldal">A téglalap a oldala, double típus</param>
+        /// <param name="bOldal">A téglalap b oldala, double típus</param>
+        public Teglalap(double aOldal, double bOldal)
+        {
+            this.aOldal = aOldal;
+            this.bOldal = bOldal;
+            TeglalapokListaja.Add(this);
+            OsszesKerulet += this.TeglalapKerulet();
+            OsszesTerulet += this.TeglalapTerulet();
+        }
+        public double TeglalapKerulet()
+        {
+            return Math.Round(2 * (this.aOldal + this.bOldal), 2);
+        }
+        public double TeglalapTerulet()
+        {
+            return Math.Round(this.aOldal * this.bOldal, 2);
         }
     }
 }
