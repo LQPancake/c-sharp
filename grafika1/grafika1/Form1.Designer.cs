f0818463463b2ec2381fbcb9dd7d7d38ffde19da
Pancake <baligajozsef@gmail.com>
2026-01-05 09:39:03 +0100

    -

---
+++ grafika1/grafika1/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace grafika1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.ButtonChange = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(353, 170);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(31, 13);
+            this.labelUsername.TabIndex = 0;
+            this.labelUsername.Text = "Color";
+            this.labelUsername.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ButtonChange
+            // 
+            this.ButtonChange.Location = new System.Drawing.Point(356, 230);
+            this.ButtonChange.Name = "ButtonChange";
+            this.ButtonChange.Size = new System.Drawing.Size(55, 23);
+            this.ButtonChange.TabIndex = 1;
+            this.ButtonChange.Text = "Módosít";
+            this.ButtonChange.UseVisualStyleBackColor = true;
+            this.ButtonChange.Click += new System.EventHandler(this.ButtonChange_Click);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(271, 204);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(238, 20);
+            this.textBoxInput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 468);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.ButtonChange);
+            this.Controls.Add(this.labelUsername);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "elso grafikus progi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.Button ButtonChange;
+        private System.Windows.Forms.TextBox textBoxInput;
+    }
+}
+
